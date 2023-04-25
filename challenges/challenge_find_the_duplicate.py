b1e001570569b856f672295c35ea1d4460a21c49
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-25 18:21:30 -0300

    feat: function to find the first duplicated number

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,9 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or nums == []:
+        return False
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1] and nums[i] > 0:
+            return nums[i]
+    return False
