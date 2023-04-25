6d138c3645c4951acb36e7ea44729d8a876638e9
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-25 14:29:41 -0300

    feat: implementing function to count students in schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,18 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    """Farei aqui meu codigo"""
-    raise NotImplementedError
+    student_counter = 0
+    if not target_time or target_time is None:
+        return None
+    for entry, end in permanence_period:
+        if (
+            entry is None
+            or end is None
+            or isinstance(entry, str)
+            or isinstance(end, str)
+        ):
+            return None
+        if (target_time > int(entry) and target_time < int(end)) or (
+            target_time == int(entry) or target_time == int(end)
+        ):
+            student_counter += 1
+    return student_counter
