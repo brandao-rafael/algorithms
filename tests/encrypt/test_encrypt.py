8073322142fba6fa278d670aa0cb8e8f74ed47d8
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-25 15:03:27 -0300

    test: for the encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "subinoonibus"
+
+    odd = encrypt_message(message, 5)
+    even = encrypt_message(message, 6)
+    inverted = encrypt_message(message, 30)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, "b")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1008, 3)
+    assert odd == "nibus_subinoo"
+    assert even == "subino_onibus"
+    assert inverted == message
